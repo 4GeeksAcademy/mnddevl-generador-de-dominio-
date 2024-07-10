6649add0d4ae5200d43e5e939f6a9d8f8b4940d8
Amanda Dias <168840059+mnddevl@users.noreply.github.com>
2024-07-10 10:12:28 +0000

    final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// Listas de pronombres, adjetivos y sustantivos
+const pronombres = ["the", "our"];
+const adjetivos = ["great", "big"];
+const sustantivos = ["jogger", "racoon"];
+const dominio = [".com", ".net"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// Array para almacenar los nombres de dominio generados
+const nombresDeDominios = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// Bucles anidados para generar todas las combinaciones posibles
+for (let p = 0; p < pronombres.length; p++) {
+  for (let a = 0; a < adjetivos.length; a++) {
+    for (let s = 0; s < sustantivos.length; s++) {
+      for (let d = 0; d < dominio.length; d++) {
+        nombresDeDominios.push(
+          pronombres[p] + adjetivos[a] + sustantivos[s] + dominio[d]
+        );
+      }
+    }
+  }
+}
+console.log(nombresDeDominios);
